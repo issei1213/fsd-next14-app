9293666f4a6605f26a784f9159db080da87b3c12
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-07-19 08:51:20 +0900

    BasicModalを少し冗長コードに変更

--- src/pages/lightning-talks/ui/modal-page.tsx
+++ src/pages/lightning-talks/ui/modal-page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { ComponentProps, useState } from 'react';
 
 import { Modal } from '@/shared/ui/compound-modal';
 import BasicModal from '@/shared/ui/modal';
@@ -61,8 +61,7 @@ const BasicModalSample = () => {
   return (
     <>
       <button
-        className="
-          select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center 
+        className="select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center 
           align-middle font-sans text-xs font-bold uppercase text-white shadow-md 
           shadow-gray-900/10 transition-all hover:shadow-lg 
           hover:shadow-gray-900/20 active:opacity-[0.85] 
@@ -73,9 +72,9 @@ const BasicModalSample = () => {
       </button>
       {modal === 'visible' && (
         <BasicModal onClose={closeModal}>
-          <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900">
+          <Heading as="h2">
             <h2>システムメンテナンスのお知らせ</h2>
-          </div>
+          </Heading>
           <div className="relative p-4 font-sans text-base antialiased font-light leading-relaxed text-blue-gray-500">
             <p>
               平素より弊社サービスをご利用いただき、誠にありがとうございます。
@@ -93,17 +92,41 @@ const BasicModalSample = () => {
           </div>
 
           <div className="flex flex-wrap items-center justify-end p-4 shrink-0 text-blue-gray-500">
-            <button
-              data-ripple-dark="true"
-              data-dialog-close="true"
-              className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              onClick={closeModal}
-            >
-              キャンセル
-            </button>
+            <Button variant="delete">キャンセル</Button>
           </div>
         </BasicModal>
       )}
     </>
+  );
+};
+
+const Heading = ({
+  children,
+  as,
+  ...props
+}: { as: 'h2' } & ComponentProps<'h1'>) => {
+  const Component = as;
+
+  return (
+    <Component
+      className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900"
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
+
+const Button = ({
+  children,
+  ...props
+}: ComponentProps<'button'> & { variant: 'delete' }) => {
+  return (
+    <button
+      className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      {...props}
+    >
+      {children}
+    </button>
   );
 };
